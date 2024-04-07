3a7782762c79c050616596dcb568801af97fcc8e
Konovalov Dmitriy <dkonowalow@gmail.com>
2024-04-07 17:09:44 +0500

    add directive

---
+++ 2_bool_and_if/23.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MUL_VAR_NAME(A, B) mul##A##_##B
+
+int main(void)
+{
+    int var_1, var_2;
+
+    if (scanf("%d %d", &var_1, &var_2) != 2)
+    {
+        printf("Invalid input");
+        return 0;
+    }
+
+    int MUL_VAR_NAME(var_1, var_2) = var_1 * var_2;
+
+    printf("%d", mul_var_1_var_2);
+
+    return 0;
+}
